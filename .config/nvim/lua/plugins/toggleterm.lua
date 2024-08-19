89a8af8aaf8962cc2b41b67296c066c7d3a7cc40
bqcuong <bqcuong2212@gmail.com>
2024-08-19 12:07:06 +0200

    update

--- .config/nvim/lua/plugins/toggleterm.lua
+++ .config/nvim/lua/plugins/toggleterm.lua
@@ -3,7 +3,7 @@ return {
     version = "*",
     config = function()
       require("toggleterm").setup {
-        open_mapping = [[<C-\>]],
+        open_mapping = [[<C-t>]],
         insert_mappings = true,
         shade_terminals = false,
         direction = 'horizontal',
