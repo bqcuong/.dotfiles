6c4e58a823bb3a0817ad6ab10ef2d687b3aa3172
bqcuong <bqcuong2212@gmail.com>
2026-08-15 22:22:38 +0200

    up

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -16,7 +16,7 @@ return {
     vim.keymap.set("n", "<leader>fk", builtin.keymaps, { desc = "Find Keymaps" })
     vim.keymap.set("n", "<leader>/", function()
       -- You can pass additional configuration to telescope to change theme, layout, etc.
-      require("telescope.builtin").current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
+      require("telescope.builtin").live_grep(require("telescope.themes").get_dropdown({
         winblend = 10,
         previewer = false,
         layout_config = { width = 0.7 },
