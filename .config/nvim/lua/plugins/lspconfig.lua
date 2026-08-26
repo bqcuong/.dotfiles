97daae49e1f178d17cd24482f9e4acd6e650d682
David Bui <david.bui@freiheit.com>
2026-08-26 13:04:42 +0200

    update nvim

---
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      vim.lsp.config("terraformls", {
+        cmd = { "terraform-ls", "serve" },
+        filetypes = { "terraform", "terraform-vars" },
+        root_markers = {
+          ".terraform",
+          ".git",
+        },
+      })
+
+      vim.lsp.enable("terraformls")
+    end,
+  },
+}
+
--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,5 +1,5 @@
 return {
-  'nvim-telescope/telescope.nvim', tag = '0.1.8',
+  'nvim-telescope/telescope.nvim', tag = 'v0.2.1',
   dependencies = {
     'nvim-lua/plenary.nvim',
     { "nvim-telescope/telescope-fzf-native.nvim", build = "make" }
@@ -7,16 +7,18 @@ return {
   config = function()
     local builtin = require("telescope.builtin")
     vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find Files" })
-
     vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Find Buffers" })
     vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Find Help Tags" })
-    vim.keymap.set("n", "<leader>fs", builtin.lsp_document_symbols, { desc = "Find Symbols" })
-    vim.keymap.set("n", "<leader>gc", builtin.git_commits, { desc = "Search Git Commits" })
-    vim.keymap.set("n", "<leader>gb", builtin.git_bcommits, { desc = "Search Git Commits for Buffer" })
     vim.keymap.set("n", "<leader>fk", builtin.keymaps, { desc = "Find Keymaps" })
+
+    vim.keymap.set("n", "<leader>fs", builtin.lsp_document_symbols, { desc = "Find Symbols" })
+    vim.keymap.set("n", "<leader>fd", builtin.lsp_definitions, { desc = "Goto Definition" })
+    vim.keymap.set("n", "<leader>fr", builtin.lsp_references, { desc = "Goto References" })
+    vim.keymap.set("n", "<leader>fi", builtin.lsp_definitions, { desc = "Goto Implementation" })
+
     vim.keymap.set("n", "<leader>/", function()
       -- You can pass additional configuration to telescope to change theme, layout, etc.
-      require("telescope.builtin").current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
+      require("telescope.builtin").live_grep(require("telescope.themes").get_dropdown({
         winblend = 10,
         previewer = false,
         layout_config = { width = 0.7 },
